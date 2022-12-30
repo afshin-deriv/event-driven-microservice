022ff6c074e9a320f6f31f718fe198513d2d6f9c
Afshin Paydar <afshin.paydar@binary.com>
2022-12-30 06:39:17 +0000

    init api-gatewa

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const redis = require('redis');
+const bodyParser = require('body-parser')
+
+const publisher = redis.createClient();
 
 const app = express();
 const port = 3000;
 
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ 
+  extended: true
+})); 
 
-app.get('/', (req, res) => {
-    res.send('Your request received!');
+app.post('/', (req, res) => {
+    (async () => { 
+        res.send('Your request received!');
+        // console.log(req.body.user-id);
+        // console.log(req.body.type);
+        // console.log(req.body.amount);
+        // console.log(req.body.symbol);
+        // await publisher.connect();
+        // await publisher.publish('trade', JSON.stringify(req));
+    })();
 });
 
 
