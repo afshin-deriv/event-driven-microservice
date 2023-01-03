4cb11b0fd120931abb166a9307d4b24272a555ee
Afshin Paydar <afshin.paydar@binary.com>
2023-01-03 08:34:34 +0000

    use Redis stream for api-gateway

--- services/api-gateway/index.js
+++ services/api-gateway/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const redis = require('redis');
+const Redis = require('ioredis');
+
+const REDIS_HOST = process.env.REDIS_HOST || 'redis';
+const REDIS_PORT = process.env.REDIS_PORT || '6379';
+require('dotenv').config();
+
+const client = new Redis({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+});
+
 const bodyParser = require('body-parser')
 const { body, validationResult } = require('express-validator');
 
-const client  = redis.createClient({ url:'redis://redis:6379' });
-const subscriber = client.duplicate();
 
 const app = express();
 const port = 3000;
@@ -14,37 +22,36 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
-(async () => {
-    await subscriber.connect();
-    await client.connect();
-})();
-
 
 app.post('/',
-// Request validation
-body('user_id').isInt(),
-body('type').isAscii(),
-body('amount').isFloat(),
-body('symbol').isAscii(),
+  // Request validation
+  body('user_id').isInt(),
+  body('type').isAscii(),
+  body('amount').isFloat(),
+  body('symbol').isAscii(),
 
-// Response generation
-(req, res) => {
+  // Response generation
+  (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-    (async () => { 
-        // console.log(req.body.user_id);
-        // console.log(req.body.type);
-        // console.log(req.body.amount);
-        // console.log(req.body.symbol);
-        await client.publish('trade', JSON.stringify(req.body));
+      (async () => {
+        const data = JSON.stringify({
+          "user_id": req.body.user_id,
+          "type": req.body.type,
+          "amount": req.body.amount,
+          "symbol": req.body.symbol
+        });
+
+        await client.xadd('api', "*", "request", data );
         res.send("OK!\n");
-    })();
+      })();
 });
 
 
 app.listen(port, () => {
     console.log(`API-Gateway is listening on port ${port}`); 
 });
+
