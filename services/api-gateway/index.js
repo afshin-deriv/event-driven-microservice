01e632c207bebad074a07c7f6c181e8f9af792b2
Afshin Paydar <afshin.paydar@binary.com>
2023-01-05 01:33:15 +0000

    refactor api-gateway

--- services/api-gateway/index.js
+++ services/api-gateway/index.js
@@ -1,62 +1,34 @@
-const Redis = require('ioredis');
 const WebSocketServer = require('ws');
+const redis = require('./redis.js');
 
-
-const REDIS_HOST = process.env.REDIS_HOST || 'redis';
-const REDIS_PORT = process.env.REDIS_PORT || '6379';
-require('dotenv').config();
-
-const client = new Redis({
-    host: REDIS_HOST,
-    port: REDIS_PORT,
-});
 
 const port = 3000;
-const STREAMS_KEY = "api";
+const wss = new WebSocketServer.Server({ port: port });
 
-(async () => {
-  client.xgroup('CREATE', 'api', 'trade', '$', 'MKSTREAM', function (err) {
-    if (err) {
-      return console.error(err);
-    }
-  });
-})();
 
-const wss = new WebSocketServer.Server({ port: port })
 wss.on("connection", ws => {
-  console.log("New ws client connected");
-  // sending message
+  console.log("New client connected");
+
   ws.on("message", data => {
       const req = JSON.parse(data);
-      // Simple validation
-      if ( req.user_id && req.type && req.amount && req.symbol ) {
+      validData = redis.validateAndParse(req);
+      if ( validData ) {
         (async () => {
-          const data = JSON.stringify({
-            "user_id": req.user_id,
-            "amount":  req.amount,
-            "symbol":  req.symbol,
-            "type":    req.type
-          });
-
-          await client.xadd(STREAMS_KEY, '*', 'request', data, function (err) {
-            if (err) {
-              return console.error(err);
-            }
-          });
+          await redis.addToStream(validData);
         })();
       } else {
         ws.send("Request format isn't valid!");
       }
       // Debug log
-      console.log(`user_id: ${obj.user_id}, type: ${obj.type}, amount: ${obj.amount}, symbol: ${obj.symbol}`);
+      console.log(`user_id: ${req.user_id}, type: ${req.type}, amount: ${req.amount}, symbol: ${req.symbol}`);
   });
 
   ws.on("close", () => {
-      console.log("The client has connected");
+      console.log("Client gets disconnected");
   });
 
-  ws.onerror = function () {
-      console.log("Some Error occurred")
+  ws.onerror = function (e) {
+      console.log("Some Error occurred", e);
   }
 });
 
---
+++ services/api-gateway/redis.js
@@ -0,0 +1,38 @@
+const Redis = require('ioredis');
+require('dotenv').config();
+
+const REDIS_HOST = process.env.REDIS_HOST || 'redis';
+const REDIS_PORT = process.env.REDIS_PORT || '6379';
+const STREAMS_KEY = "api";
+
+const client = new Redis({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+});
+
+
+const redis = {
+
+    addToStream: function (data) { 
+        client.xadd(STREAMS_KEY, '*', 'request', data, function (err) {
+            if (err) {
+              return console.error(err);
+            }
+          });
+    },
+
+    validateAndParse: function (data) {
+        if ( data.user_id && data.type && data.amount && data.symbol ) {
+            const jsonData = JSON.stringify({
+                "user_id": data.user_id,
+                "amount":  data.amount,
+                "symbol":  data.symbol,
+                "type":    data.type
+              });
+	    return jsonData;
+	}
+        return;
+    }
+};
+
+module.exports = redis;
